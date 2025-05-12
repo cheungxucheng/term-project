164e7e612218b87d707710de2c804cf020a014ae
cheungxucheng <ryanxzhang2.0@gmail.com>
2025-05-12 16:00:23 -0700

    product pages now get products from the db, checkout and product pages also change based on auth

--- src/routes/product/product.js
+++ src/routes/product/product.js
@@ -1,11 +1,25 @@
 const express = require('express');
+const db = require('../../config/db');
 const router = express.Router();
+const { getProducts } = require('./../../config/db.prepare');
+const auth = require('../../middleware/auth');
 
 router.get('/:id', (req, res) => {
     const productId = parseInt(req.params.id);
-    const product = products.find(p => p.id === productId);
-    if (!product) return res.status(404).send('Product not found');
-    res.render('product', { product });
+    db.serialize(() => {
+        db.get('SELECT * FROM Products WHERE id = ?', productId, (err, product) => {
+            if (err) {
+                console.error('Error fetching product:', err.message);
+                return res.status(500).json({ error: 'Internal Server Error' });
+            }
+            else if (!product) {
+                        return res.status(404).send('Product not found');
+            }
+            else {
+                res.render('product', { product });
+            }
+        })
+    })
 });
 
 module.exports = router;
